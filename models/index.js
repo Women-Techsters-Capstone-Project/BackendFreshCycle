8c2971647ba8dfb1f70727beaa46e2136e0d79a6
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2026-02-28 11:30:28 +0000

    Changes to the backend

--- models/index.js
+++ models/index.js
@@ -3,26 +3,31 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const process = require('process');
+require('dotenv').config(); // <- Make sure this is at the very top
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// Read DB config from .env
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: 'mysql',
+    logging: false, // optional: disables SQL logging
+  }
+);
 
+// Load all models in this directory
 fs
   .readdirSync(__dirname)
   .filter(file => {
     return (
       file.indexOf('.') !== 0 &&
-  file !== basename &&
-  file !== 'associations.js' &&
+      file !== basename &&
+      file !== 'associations.js' &&
       file.slice(-3) === '.js' &&
       file.indexOf('.test.js') === -1
     );
@@ -32,12 +37,12 @@ fs
     db[model.name] = model;
   });
 
+// Run model associations if defined
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
-
 // Models were loaded from the files in this directory above. Ensure we use those
 // model instances (db.User, db.Produce, etc.) instead of migration files.
 
